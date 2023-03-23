40c6f3aa51e223fb4cec1d108f8d048dd52b606c
Md. Mazharul Islam <mipiyas07@gmail.com>
2023-03-24 01:40:25 +0600

    dot vs bracket notation

---
+++ notation.js
@@ -0,0 +1,23 @@
+const person = {
+    name: 'korim chowdhury',
+    profession:'traffic Surgeon',
+    age:39,
+    25: 'summer',
+    'son-name':'Sunny',
+    address:'banglamotor',
+}
+//dot notation
+const prof1 = person.profession;
+//console.log(prof1);
+
+//bracket notation
+
+const prof2 = person['profession'];
+const pName = 'profession';
+const prof3 = person[pName];
+//console.log(prof3);
+
+const season = person[25];
+//console.log(season);
+const son = person["son-name"];
+
